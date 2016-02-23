b3df63a3ee02048c762b3f3566c707b4034d8e7b
Charles Eyermann <Charles@eduroam-171-240.dyn.carleton.edu>
2016-02-22 21:50:54 -0600

    finished simulator

--- Assembly Project/simulator.py
+++ Assembly Project/simulator.py
@@ -3,26 +3,105 @@
 
 
 import numpy as np
-import sys, os, collections
+import sys, os, collections, random
 
 class Simulator:
 
-	def __init__(self, input_file):
+	def __init__(self, input_file,coverage,read_length,error_rate):
 		self.raw_data = ""
 		self.header = ""
-
+		self.coverage = coverage # coverage =  NL/G
+		self.read_length = read_length
+		self.error_rate = error_rate
+		
 		with open(input_file, 'rU') as f:
 			for row in f:
 				if row[0] == ">":
-					self.header += row
+					self.header += row.strip()
 				else:
-					self.raw_data += row
-			
+					self.raw_data += row.strip()
+	
+		self.G = len(self.raw_data)
+		self.N = (self.coverage * self.G)/self.read_length
+		self.NL = self.N * self.read_length
+		self.reads = []
+		self.mutation_count = 0
+
+	def mutate(self, c):
+		n = ['A','C','G','T']
+		n.remove(c)
+		return random.choice(n)
+
+	def generate_random_nucleotide(self):
+		n = ['A','C','G','T']
+		return random.choice(n)
+
+	def mangle_data(self, sequence):
+		#change error rate to an int from 1-100 to make it easier for python's random number generator
+		error_percent = 100*self.error_rate
+
+		for i in range(len(sequence)): #for nucleotide in sequence: s[0] to s[19] i range 1 to 19
+			# roll the dice
+			error = random.randint(1,100)
+			# check to see if mutation occured
+			if error <= error_percent:
+				#print "mutation occured at position i: ", i
+				
+				# edge case if mutation occurs in first index of string
+				if i == 0:
+					#print "<"+self.mutate(sequence[i])+">"+sequence[1:]
+					sequence = self.mutate(sequence[i]) + sequence[1:]
+					self.mutation_count += 1
+				
+				# edge case if mutation occurs in second index of string
+				elif i == 1:
+					#print sequence[0]+"<"+self.mutate(sequence[1])+">"+sequence[2:]
+					sequence = sequence[0] + self.mutate(sequence[1]) + sequence[2:]
+					self.mutation_count += 1
+
+				# edge case if mutation occurs in second to last index of string
+				elif i == (len(sequence) - 2):
+					#print sequence[:-3]+"<"+self.mutate(sequence[i])+">"+sequence[-1:]
+					sequence = sequence[:-3] + self.mutate(sequence[i]) + sequence[-1:]
+					self.mutation_count += 1
+
+				# edge case if mutation occurs in last index of string
+				elif i == (len(sequence) - 1):
+					#print sequence[:i]+"<"+self.mutate(sequence[i])+">"
+					sequence[:i] + self.mutate(sequence[i])
+					self.mutation_count += 1
+
+				# handle non-edge cases
+				else:
+					if len(sequence) == self.read_length:
+						#print sequence[:i]+"<"+self.mutate(sequence[i])+">"+sequence[i+1:]	
+						sequence = sequence[:i] + self.mutate(sequence[i]) + sequence[i+1:]
+						self.mutation_count += 1
+						
+		return sequence
+
+	def generate_reads(self):
+		index_range = self.G - self.read_length - 1
+		for i in range(self.N):
+			read_start_index = random.randint(0, index_range)
+			read_end_index = read_start_index + self.read_length
+			seq = self.raw_data[read_start_index:read_end_index]
+			mutated_seq = self.mangle_data(seq)
+			self.reads.append(mutated_seq)
+		
+		return self.reads
 
 
 
 
-sequence = Simulator("sample.fasta.txt")
-print sequence.header
+
+sequences = Simulator("sample.fasta.txt", 3, 20, 0.01)
+print sequences.header
+#print sequence.raw_data
+print sequences.G
+print sequences.N
+
+print sequences.generate_reads()
+print sequences.mutation_count
 
 
